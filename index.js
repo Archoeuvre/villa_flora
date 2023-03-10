d9eaf59c80d9f2659e04f3971a6334371f6b4b53
Archoeuvre <archoeuvre.design@gmail.com>
2023-03-10 17:04:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+AOS.init();
+
+const menuBtn =document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click",()=>{
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
